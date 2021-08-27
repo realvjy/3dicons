048aa596d3bb11d264523dd4649522856b829268
realvjy <realvjy@gmail.com>
2021-08-28 02:17:35 +0530

    Updates

--- component---src-pages-404-js-8ec69e322cf376ad31b3.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk_3Dicons=self.webpackChunk_3Dicons||[]).push([[883],{9616:function(t,e,n){"use strict";n.r(e);var a=n(7294),l=n(7198),o=n(3751);e.default=function(t){var e=t.data,n=t.location,s=e.site.siteMetadata.title;return a.createElement(l.Z,{location:n,title:s},a.createElement(o.Z,{title:"404: Not Found"}),a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-8ec69e322cf376ad31b3.js.map
---
+++ component---src-pages-404-js-92539edae920963c8a49.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_3Dicons=self.webpackChunk_3Dicons||[]).push([[883],{9616:function(t,e,n){n.r(e);var a=n(7294),l=n(7198),o=n(3751);e.default=function(t){var e=t.data,n=t.location,s=e.site.siteMetadata.title;return a.createElement(l.Z,{location:n,title:s},a.createElement(o.Z,{title:"404: Not Found"}),a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-92539edae920963c8a49.js.map
