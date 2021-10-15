47017486c1e99d99f18bd77dc09e9d144dbe736a
realvjy <realvjy@gmail.com>
2021-10-15 21:54:35 +0530

    #22 Badage update

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,7 +18,7 @@ const Layout = ({ location, title, children }) => {
           <header>{header}</header>
           <main>{children}</main>
           <div className="ph-float">
-            <a href="https://www.producthunt.com/posts/3dicons?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-3dicons" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=315795&theme=dark" alt="3dicons - Open source 3D icon library with over 1440+ icons | Product Hunt"/></a>
+            <a href="https://www.producthunt.com/posts/3dicons?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-3dicons" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=315795&theme=dark&period=daily" alt="3dicons - Open source 3D icon library with over 1440+ icons | Product Hunt"/></a>
           </div>
         </div>
     )
