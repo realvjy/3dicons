c9cd6834cbb98fd1b74b3b7e569e8fe20b153cfc
realvjy <realvjy@gmail.com>
2021-11-21 22:26:13 +0530

    ad link test

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,6 +20,12 @@ const Layout = ({ location, title, children }) => {
           <div className="ph-float">
             <a href="https://www.producthunt.com/posts/3dicons?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-3dicons" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=315795&theme=dark&period=daily" alt="3dicons - Open source 3D icon library with over 1440+ icons | Product Hunt"/></a>
           </div>
+
+          <div className="ad-float">
+          <a href="https://bit.ly/icon-shock-3dicons" target="_blank">
+            <img src="iconshock.png" />
+          </a>
+          </div>
         </div>
     )
 }
