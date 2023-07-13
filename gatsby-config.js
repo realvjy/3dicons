2f621742466439e133ab1d6095f83a32c3769706
realvjy <realvjy@gmail.com>
2023-07-13 07:45:19 +0530

    fix ga4

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,9 +69,15 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: `UA-151947591-2`,
+        trackingIds: [
+          "G-XMCL0RJMF8", // Google Analytics / GA
+        ],
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true
+        },
       },
     },
     {
